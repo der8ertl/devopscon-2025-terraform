9632c1fc67aadf52d174fa807f5a20cd0a4d57f9
Stefan <121409136+der8ertl@users.noreply.github.com>
2025-06-19 12:11:19 +0000

    update main and variables

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+    description = "Azure Subscription ID"
+    type = string
+}
+
+variable "resource_group_name" {
+    description = "Name of resource group"
+    type = string
+}
+
+variable "resource_group_location" {
+    description = "Location of resource group"
+    type = string
+}
+
